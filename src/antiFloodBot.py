3f3c341bec0927135b7c0e3fb8496aa71e6c5cdb
sztart@gmail.com <sztart@gmail.com>
2014-02-16 00:49:00 +0100

    One more update

--- src/antiFloodBot.py
+++ src/antiFloodBot.py
@@ -32,11 +32,11 @@ def antiFlood(inp, nick=None, msg=None, conn=None, chan=None):
 
     inputs[nick] = filter(lambda x: x > timeThreshold, inputs[nick]) #filter out every entry older than 8 sec (threshold)
 
-    if (len(inputs[nick]) >= entryThreshold): #if user has good day, kick one
+    if len(inputs[nick]) >= entryThreshold: #if user has good day, kick one
         explanationMessage = conf['kickMessage']
         file = open(logFile, 'a')
         file.write('Trying to kick %s on channel %s \n' % (nick, chan))
-        if (nick in kicked):
+        if nick in kicked:
             explanationMessage =  conf['banMessage']
             out = "MODE %s +b %s" % (chan, nick)
             conn.send(out)
@@ -51,3 +51,10 @@ def antiFlood(inp, nick=None, msg=None, conn=None, chan=None):
 #todo
 #if the same user joins again within 24 hour and keeps spamming temp ban in XX time.
 #step 3) if the same user joins after the removal of the ban and spams, permanent ban.
+
+
+@hook.event("NICK")
+def onnick(param, conn=None, raw=None):
+    file = open(logFile, 'a')
+    file.write(json.dumps(param))
+    file.close()
